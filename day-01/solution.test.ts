f4f9b28e886ff522828780f9400b1337c51aa9a3
Miloritam <139753763+Miloritam@users.noreply.github.com>
2023-09-26 23:06:38 +0200

    Banana tree

--- day-01/solution.test.ts
+++ day-01/solution.test.ts
@@ -6,7 +6,7 @@ import { solvePart1, solvePart2 } from "./solution";
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8');
 
 test('part 1', () => {
-    expect(solvePart1(input)).toEqual(68923);
+    expect(solvePart1(input)).toEqual(68925);
 });
 
 test('part 2', () => {
